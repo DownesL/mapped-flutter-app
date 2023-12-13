051d70a546e44f6b8b19550ff4c17b14722d58a9
DownieTown <95034909+DownieTown@users.noreply.github.com>
2023-12-13 12:18:05 +0100

    update event sheet after remove/add image

--- lib/widgets/mediors/event_sheet.dart
+++ lib/widgets/mediors/event_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:mapped/firebase_service.dart';
 import 'package:mapped/models/event.dart';
 import 'package:mapped/models/mapped_user.dart';
 import 'package:mapped/widgets/mediors/add_image_overlay.dart';
@@ -35,6 +36,8 @@ class _EventSheetState extends State<EventSheet> {
   final nameController = TextEditingController();
   final locationController = TextEditingController();
 
+  final fS = FirebaseService();
+
   _showAddImageDialog(BuildContext context) async {
     overlayEntry = OverlayEntry(
       builder: (context) {
@@ -60,9 +63,19 @@ class _EventSheetState extends State<EventSheet> {
     super.initState();
   }
 
+  bool isLoading = false;
+
+  void getEvent() async {
+    var e = await fS.getEventByID(widget.event.eid);
+    if (e == null) return;
+    event = e;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     var mUser = context.watch<MappedUser>();
+    getEvent();
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
@@ -115,42 +128,42 @@ class _EventSheetState extends State<EventSheet> {
               ),
             ],
           ),
-          SizedBox(
+          const SizedBox(
             height: 8.0,
           ),
           if (event.pictureList.isNotEmpty)
             Flexible(
-                fit: FlexFit.loose,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  itemCount: event.pictureList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      decoration:
-                          BoxDecoration(borderRadius: BorderRadius.circular(5)),
-                      margin: EdgeInsets.only(right: 4.0),
-                      clipBehavior: Clip.antiAlias,
-                      child: Image(
-                        width: 150,
-                        height: 200,
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          event.pictureList[index],
-                        ),
+              fit: FlexFit.loose,
+              flex: 2,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: event.pictureList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                    margin: const EdgeInsets.only(right: 4.0),
+                    clipBehavior: Clip.antiAlias,
+                    child: Image(
+                      width: 200,
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        event.pictureList[index],
                       ),
-                    );
-                  },
-                ))
+                    ),
+                  );
+                },
+              ),
+            )
           else
             const SizedBox(
-                height: 150, child: Center(child: Text('No pictures yet'))),
-          const Spacer(),
+                height: 250, child: Center(child: Text('No pictures yet'))),
+          const Spacer(flex: 1),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               QRCodePopup(url: 'events/${event.eid}'),
-              Spacer(),
+              const Spacer(),
               IconButton.outlined(
                 style: ButtonStyle(
                   side: MaterialStatePropertyAll(
@@ -166,46 +179,60 @@ class _EventSheetState extends State<EventSheet> {
                 ),
               ),
               if (event.organiserIDs.contains(mUser.uid))
-                const SizedBox(
-                  width: 4,
-                ),
-              if (event.organiserIDs.contains(mUser.uid))
-                OutlinedButton.icon(
-                  onPressed: () => _showAddImageDialog(context),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    OutlinedButton.icon(
+                      onPressed: () => _showAddImageDialog(context),
+                      style: ButtonStyle(
+                        side: MaterialStatePropertyAll(
+                          BorderSide(color: Theme.of(context).primaryColor),
+                        ),
+                      ),
+                      icon: const Icon(
+                        Icons.add_a_photo,
+                        size: 16,
+                      ),
+                      label: const Text("Add"),
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Edit event"),
+                      style: ButtonStyle(
+                          foregroundColor: MaterialStatePropertyAll(
+                              Theme.of(context).colorScheme.tertiary),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  side: BorderSide(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .tertiary)))),
+                    )
+                  ],
+                )
+              else if (event.attendeeIDs.contains(mUser.uid))
+                TextButton(
                   style: ButtonStyle(
                     side: MaterialStatePropertyAll(
                       BorderSide(color: Theme.of(context).primaryColor),
                     ),
                   ),
-                  icon: const Icon(
-                    Icons.add_a_photo,
-                    size: 16,
-                  ),
-                  label: Text("Add"),
-                ),
-              if (event.organiserIDs.contains(mUser.uid))
-                const SizedBox(
-                  width: 4,
-                ),
-              if (event.organiserIDs.contains(mUser.uid))
-                TextButton(
-                  onPressed: () {},
-                  child: const Text("Edit event"),
-                  style: ButtonStyle(
-                      foregroundColor: MaterialStatePropertyAll(
-                          Theme.of(context).colorScheme.tertiary),
-                      shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                          side: BorderSide(
-                              color: Theme.of(context).colorScheme.tertiary)))),
-                )
-              else if (event.attendeeIDs.contains(mUser.uid))
-                TextButton(
                   onPressed: () {},
                   child: const Text('Leave Event'),
                 )
               else
                 TextButton(
+                  style: ButtonStyle(
+                    side: MaterialStatePropertyAll(
+                      BorderSide(color: Theme.of(context).primaryColor),
+                    ),
+                  ),
                   onPressed: () {},
                   child: const Text('Join Event'),
                 ),
